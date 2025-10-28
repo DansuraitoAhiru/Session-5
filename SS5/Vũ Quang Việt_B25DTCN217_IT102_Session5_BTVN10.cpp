9c09ebfa27a20a553bdb30fb91c1a836a9ed34a9
DansuraitoAhiru <vqviet2007@gmail.com>
2025-10-28 14:13:32 +0700

    Update Vũ Quang Việt_B25DTCN217_IT102_Session5_BTVN10.cpp

--- SS5/Vũ Quang Việt_B25DTCN217_IT102_Session5_BTVN10.cpp
+++ SS5/Vũ Quang Việt_B25DTCN217_IT102_Session5_BTVN10.cpp
@@ -1,41 +1,43 @@
 #include <stdio.h>
 #include <math.h>
+#include <math.h>
 int main(){
 	float phuPhi, tienBacthang, TienDien;
 	int kwh, loaiHo;
+	float phuPhi1=0.05, phuPhi2=0.1, phuPhi3=0.08;
+	float tienBac1=1.5, tienBac2=2.0, tienBac3=2.5, tienBac4=3.0;
 	printf("Nhap so dien tieu thu: ");
 	scanf("%d", &kwh);
 	printf("Nhap loai ho tieu dung: ");
 	scanf("%d", &loaiHo);
-	
-	if(loaiHo<1 || loaiHo>3){
-	   printf("Loai ho tieu ko hop le");
-	   return 0;
-	}
-	
-	if(loaiHo==1)
-	   phuPhi=tienBacthang*0.05;
-	else if(loaiHo==2)
-	   phuPhi=tienBacthang*0.1;
-	else if(loaiHo==3)
-	   phuPhi=tienBacthang*0.08;
-	   
 	if(kwh<0){
 	    printf("So dien tieu thu ko the nho hon 0");
 	    return 0;
 	}
 	
 	if(kwh<=50)
-	    tienBacthang=kwh*1500;
+	    tienBacthang=kwh*tienBac1;
 	else if(kwh<=100)
-	    tienBacthang=50*1500 + (kwh-50)*2000;
+	    tienBacthang=50*tienBac1 + (kwh-50)*tienBac2;
 	else if(kwh<=200)
-	    tienBacthang=50*1500+50*2000 + (kwh-100)*2500;
+	    tienBacthang=50*tienBac1+50*tienBac2 + (kwh-100)*tienBac3;
 	else 
-	    tienBacthang=50*1500+50*2000+100*2500+(kwh-200)*3000;
+	    tienBacthang=50*tienBac1+50*tienBac2+100*tienBac3+(kwh-200)*tienBac4;
+	    
+	if(loaiHo<1 || loaiHo>3){
+	   printf("Loai ho tieu ko hop le");
+	   return 0;
+	}
 	
+	if(loaiHo==1)
+	   phuPhi=tienBacthang*phuPhi1;
+	else if(loaiHo==2)
+	   phuPhi=tienBacthang*phuPhi2;
+	else if(loaiHo==3)
+	   phuPhi=tienBacthang*phuPhi3;
+	   
 	TienDien=tienBacthang+phuPhi;
-	printf("Tien dien = %.2f VND", TienDien);
+	printf("Tien dien = %.3f VND", TienDien);
 	return 0;
 }
-	
+
